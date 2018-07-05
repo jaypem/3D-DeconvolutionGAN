08b4024a09a9c038cc910cb847461f610123e8e1
PhilippP <jan-philipp.mueller@uni-jena.de>
2018-07-05 20:32:32 +0200

    remove unnecessary methods, implement 3D convolution, add noise functions, develop Deconvolution model/class

--- scripts/deconvolution.py
+++ scripts/deconvolution.py
@@ -1,10 +1,12 @@
 import numpy as np
-# from numpy.fft import fft2, ifft2
 import cv2
 from skimage import restoration
 import tensorflow as tf
+import sys
+import os
+# from keras.models import Model
 
-def conv(img, f_type, radius_perc, k_size=5, show_mask=False):
+def conv2d(img, f_type, radius_perc, k_size=5, show_mask=False):
     if f_type == 'gaussian':
         img_back = cv2.GaussianBlur(img, (k_size,k_size), 1)
     elif f_type == 'ft_low_pass':
@@ -32,58 +34,113 @@ def conv(img, f_type, radius_perc, k_size=5, show_mask=False):
 
     return img_back
 
-def up_size_3D(img, factor):
-    f = np.fft.fft2(img)
-    fshift = np.fft.fftshift(f)
+def abssqr(vol):
+    # get the absolute square of a complex numbers
+    return np.real(vol*np.conj(vol))
 
-    print(fshift.shape)
+def conv3d_fft(vol, otf):
+    vol_fft = np.fft.fftn(vol)
+    vol_fftshift = np.fft.fftshift(vol_fft)
 
-def down_size_3D(img, factor):
-    f = np.fft.fft2(img)
-    fshift = np.fft.fftshift(f)
+    vol_fftshift = np.multiply(vol_fftshift, otf)
 
-    print(fshift.shape)
+    vol_fftshift = np.fft.ifftshift(vol_fftshift)
+    vol_fft = np.fft.ifftn(vol_fftshift)
+    return abssqr(vol_fft)
+
+# TODO: das hier fertig machen (shift von sönke holen)
+# def conv3d_fft_tf(vol, otf):
+#     fft_otf = tf.Variable(otf)
+#     input = tf.Variable(vol)
+#     input = tf.cast(input, dtype=tf.complex64)
+#     vol_fft = tf.spectral.fft3d(input)
+#
+#     init = tf.global_variables_initializer()
+#     with tf.Session() as sess:
+#         sess.run(init)
+#         vol_fftshift = sess.run(vol_fft)
+#         vol_fftshift = np.fft.fftshift(vol_fftshift)
+#         # mul = tf.multiply(x=tf.Variable(vol_fftshift), y=fft_otf)
+#         # conv = sess.run(mul)
+#
+#     #vol_fft = sess.run(vol_fft) #vol_fft.eval()
+#     # # vol_fftshift = np.fft.fftshift(vol_fft)
+#     # # vol_fftshift = np.multiply(vol_fftshift, otf)
+#     # # # vol_fftshift = vol_fft_crop.eval()
+#     # # vol_fftshift = np.fft.ifftshift(vol_fftshift)
+#     # # # vol_fft = tf.Variable(vol_fftshift)
+#     # # vol_fft = np.fft.ifftn(vol_fftshift)
+#     # # # output = tf.spectral.ifft3d(vol_fft)    #
+#     # # # output = tf.abs(output)
+#     # # # output = tf.round(output)
+#
+#     output = 0
+#
+#     return output
+
+def add_poisson(vol):
+    NPhot = 100
+    vol_output = vol.astype(float)/np.max(vol)*NPhot
+    return np.random.poisson(vol_output)
+
+# TODO: keinen Filter sondern gauss verteiltes rauschen drüber legen
+def add_gaussian(vol, sigma=2):
+    from scipy.ndimage.filters import gaussian_filter
+    return gaussian_filter(vol, sigma=sigma)
+
+def add_shift(vol):
+    import cv2
+    num_rows, num_cols = vol.shape[:2]
+    x, y = np.random.randint(low=5, high=20, size=2)
+    translation_matrix = np.float32([ [1,0,x], [0,1,y] ])
+    return cv2.warpAffine(vol, translation_matrix, (num_cols, num_rows))
 
 
-class Deconv():
-    def __init__(self, size, gan, dim='2D'):
+class Deconvolution():
+    def __init__(self, size, gan, psf, dim='3D'):
         self.gan = gan
         self.dim = dim
+        self.psf = psf
         if self.dim == '2D':
-            self.input_shape = (size,size)
+            self.input_shape = (size[0],size[1])
         elif self.dim == '3D':
-            self.input_shape = (size,size,size)
+            self.input_shape = (size[0],size[1],size[2])
         else:
             print('no suitable dimension')
 
+        self.model = self.create_model()
+
     def create_model(self):
         '''
-            model: dF/dx = min( |A - H(x)|^2 + lamda + Discriminator(x) )
+            model: dF/dx = min( |A - H(x)|^2 + lamda + (1 - gan.Discriminator(x)) )
 
-            A: measurement
-            H: PSF-convolution
-            x: sample image
-            lamda:
+            A:                  measurement
+            H:                  PSF-convolution (default OTF)
+            x:                  sample image
+            lamda:              total variation regularization
+            gan.Discriminator:  train GAN discriminator
         '''
 
         with tf.variable_scope("deconvolution_model_parameter"):
             A = tf.placeholder(tf.float32, shape=self.input_shape)
+            H = tf.placeholder(tf.float32, shape=self.input_shape)
             x = tf.placeholder(tf.float32, shape=self.input_shape)
             lamda = tf.placeholder(tf.float32, shape=(1,))
-            if self.dim == '2D':
-                H = 0 # Funktion mit Faltung der PSF tf.conv2d(...)
-            else:
-                H = 0 # Funktion mit Faltung der PSF tf.conv3d(...)
-            D = 0 # Modell des Discriminator mit prediction auf x
+            D = 0 # self.gan # Modell des Discriminator mit prediction auf x
 
         with tf.name_scope("deconvolution_model"):
-            model = tf.square(tf.abs(A-x)) + lamda + D #H(x) # D.predict(x)
+            model = tf.norm(tf.abs(A-x), ord='euclidean', axis=[-2,-1], name='deconv_norm') + lamda + D.predict(x) #H(x)
+            # model = tf.norm(tf.abs(A-x), ord='fro', axis=[-2,-1], name='deconv_norm')
+            #             + lamda + D.predict(x) #H(x)
 
         optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.1)
 
+    def minimize(self):
+        pass
 
 
-# sonstiges
+
+### SONSTIGES ###
 
 def wiener(img, psf, balance):
     return restoration.wiener(img, psf, 1, clip=False)
@@ -102,27 +159,3 @@ def wiener_filter_sw(img, kernel, K = 10):
     dummy = dummy * kernel
     dummy = np.abs(ifft2(dummy))
     return np.uint8(dummy)
-
-def blur_edge(img, d=31):
-    h, w  = img.shape[:2]
-    img_pad = cv2.copyMakeBorder(img, d, d, d, d, cv2.BORDER_WRAP)
-    img_blur = cv2.GaussianBlur(img_pad, (2*d+1, 2*d+1), -1)[d:-d,d:-d]
-    y, x = np.indices((h, w))
-    dist = np.dstack([x, w-x-1, y, h-y-1]).min(-1)
-    w = np.minimum(np.float32(dist)/d, 1.0)
-    return img*w + img_blur*(1-w)
-
-def motion_kernel(angle, d, sz=65):
-    kern = np.ones((1, d), np.float32)
-    c, s = np.cos(angle), np.sin(angle)
-    A = np.float32([[c, -s, 0], [s, c, 0]])
-    sz2 = sz // 2
-    A[:,2] = (sz2, sz2) - np.dot(A[:,:2], ((d-1)*0.5, 0))
-    kern = cv2.warpAffine(kern, A, (sz, sz), flags=cv2.INTER_CUBIC)
-    return kern
-
-def defocus_kernel(d, sz=65):
-    kern = np.zeros((sz, sz), np.uint8)
-    cv2.circle(kern, (sz, sz), d, 255, -1, cv2.LINE_AA, shift=1)
-    kern = np.float32(kern) / 255.0
-    return kern
