cdc872f78cd9b8a1e077dc93015cddc54c0e400c
PhilippP <jan-philipp.mueller@uni-jena.de>
2018-08-14 09:55:33 +0200

    rename file, add class for interactive plot of 3D arrays

--- scripts/plot3DVol.py
+++ scripts/plot3DVol.py
@@ -1,7 +1,49 @@
+# -*- coding: utf-8 -*-
+
 import numpy as np
 import matplotlib.pyplot as plt
 from plotly.offline import plot
 
+class Interactive_3DVolume():
+    def __init__(self, vol):
+        self.volume = vol.T
+        print('volume shapes before / after: {} / {}'.format(vol.shape, self.volume.shape))
+        print('for change the stack use keys: {k,j}')
+
+    def remove_keymap_conflicts(self, new_keys_set):
+        for prop in plt.rcParams:
+            if prop.startswith('keymap.'):
+                keys = plt.rcParams[prop]
+                remove_list = set(keys) & new_keys_set
+                for key in remove_list:
+                    keys.remove(key)
+
+    def process_key(self, event):
+        fig = event.canvas.figure
+        ax = fig.axes[0]
+        if event.key == 'j':
+            self.__previous_slice(ax)
+        elif event.key == 'k':
+            self.__next_slice(ax)
+        fig.canvas.draw()
+
+    def __previous_slice(self, ax):
+        volume = ax.volume
+        ax.index = (ax.index - 1) % volume.shape[0]  # wrap around using %
+        ax.images[0].set_array(volume[ax.index])
+
+    def __next_slice(self, ax):
+        volume = ax.volume
+        ax.index = (ax.index + 1) % volume.shape[0]
+        ax.images[0].set_array(volume[ax.index])
+
+    def multi_slice_viewer(self):
+        self.remove_keymap_conflicts({'j', 'k'})
+        fig, ax = plt.subplots()
+        ax.volume = self.volume
+        ax.index = self.volume.shape[0] // 2
+        ax.imshow(self.volume[ax.index], cmap='gray')
+        fig.canvas.mpl_connect('key_press_event', self.process_key)
 
 class CellVolume():
     def __init__(self, vol_type, vol_size, offset=10, draw_dist=False):
