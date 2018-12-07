f5803a8eac84dcc066495ffb1d8c7cca8e49dfef
PhilippP <jan-philipp.mueller@uni-jena.de>
2018-12-07 21:08:22 +0100

    further developement of tensorflow implementation of vol2vol

--- scripts/vol2vol_tf.py
+++ scripts/vol2vol_tf.py
@@ -120,7 +120,7 @@ def gen_conv(batch_input, out_channels):
     # if a.separable_conv: # not used, because of "no color channel" => gray value images
     #     return tf.layers.separable_conv2d(batch_input, out_channels, kernel_size=4, strides=(2, 2), padding="same", depthwise_initializer=initializer, pointwise_initializer=initializer)
     # else:
-    return tf.layers.conv2d(batch_input, out_channels, kernel_size=3, strides=(2, 2, 2), padding="same", kernel_initializer=initializer)
+    return tf.layers.conv3d(batch_input, out_channels, kernel_size=3, strides=(2, 2, 2), padding="same", kernel_initializer=initializer)
 
 
 # def gen_deconv(batch_input, out_channels):
@@ -405,17 +405,17 @@ def load_examples(otf):
         steps_per_epoch=steps_per_epoch,
     )
 
-############### BIS HIER HER GEKOMMEN! ##################
 
 def create_generator(generator_inputs, generator_outputs_channels):
     layers = []
 
-    # encoder_1: [batch, 256, 256, in_channels] => [batch, 128, 128, ngf]
+    # encoder_1: [batch, 256, 256, in_channels] => [batch, 128, 128, ngf] # 2D
+    # encoder_1: [batch, 256, 256, 256, in_channels] => [batch, 128, 128, 128 ngf] # assign vol shape
     with tf.variable_scope("encoder_1"):
         output = gen_conv(generator_inputs, a.ngf)
         layers.append(output)
 
-    layer_specs = [
+    layer_specs = [ # add one dimension in comment for depth like 'encoder_1'
         a.ngf * 2, # encoder_2: [batch, 128, 128, ngf] => [batch, 64, 64, ngf * 2]
         a.ngf * 4, # encoder_3: [batch, 64, 64, ngf * 2] => [batch, 32, 32, ngf * 4]
         a.ngf * 8, # encoder_4: [batch, 32, 32, ngf * 4] => [batch, 16, 16, ngf * 8]
@@ -427,13 +427,16 @@ def create_generator(generator_inputs, generator_outputs_channels):
 
     for out_channels in layer_specs:
         with tf.variable_scope("encoder_%d" % (len(layers) + 1)):
-            rectified = lrelu(layers[-1], 0.2)
-            # [batch, in_height, in_width, in_channels] => [batch, in_height/2, in_width/2, out_channels]
+            # rectified = lrelu(layers[-1], 0.2)
+            rectified = rainersReLU(layers[-1])
+
+            # [batch, in_height, in_width, in_channels] => [batch, in_height/2, in_width/2, out_channels] # 2D
+            # [batch, in_height, in_width, in_depth, in_channels] => [batch, in_height/2, in_width/2, in_depth/2, out_channels]
             convolved = gen_conv(rectified, out_channels)
             output = batchnorm(convolved)
             layers.append(output)
 
-    layer_specs = [
+    layer_specs = [         # add one dimension in comment for depth like 'decoder_1'
         (a.ngf * 8, 0.5),   # decoder_8: [batch, 1, 1, ngf * 8] => [batch, 2, 2, ngf * 8 * 2]
         (a.ngf * 8, 0.5),   # decoder_7: [batch, 2, 2, ngf * 8 * 2] => [batch, 4, 4, ngf * 8 * 2]
         (a.ngf * 8, 0.5),   # decoder_6: [batch, 4, 4, ngf * 8 * 2] => [batch, 8, 8, ngf * 8 * 2]
@@ -452,7 +455,8 @@ def create_generator(generator_inputs, generator_outputs_channels):
                 # since it is directly connected to the skip_layer
                 input = layers[-1]
             else:
-                input = tf.concat([layers[-1], layers[skip_layer]], axis=3)
+                # input = tf.concat([layers[-1], layers[skip_layer]], axis=3)
+                input = tf.concat([layers[-1], layers[skip_layer]], axis=4)
 
             rectified = tf.nn.relu(input)
             # [batch, in_height, in_width, in_channels] => [batch, in_height*2, in_width*2, out_channels]
@@ -464,9 +468,11 @@ def create_generator(generator_inputs, generator_outputs_channels):
 
             layers.append(output)
 
-    # decoder_1: [batch, 128, 128, ngf * 2] => [batch, 256, 256, generator_outputs_channels]
+    # decoder_1: [batch, 128, 128, ngf * 2] => [batch, 256, 256, generator_outputs_channels] # 2D
+    # decoder_1: [batch, 128, 128, 128, ngf * 2] => [batch, 256, 256, 256, generator_outputs_channels] # assign vol shape
     with tf.variable_scope("decoder_1"):
-        input = tf.concat([layers[-1], layers[0]], axis=3)
+        # input = tf.concat([layers[-1], layers[0]], axis=3)
+        input = tf.concat([layers[-1], layers[0]], axis=4)
         rectified = tf.nn.relu(input)
         output = gen_deconv(rectified, generator_outputs_channels)
         output = tf.tanh(output)
@@ -480,13 +486,17 @@ def create_model(inputs, targets):
         n_layers = 3
         layers = []
 
-        # 2x [batch, height, width, in_channels] => [batch, height, width, in_channels * 2]
-        input = tf.concat([discrim_inputs, discrim_targets], axis=3)
+        # 2x [batch, height, width, in_channels] => [batch, height, width, in_channels * 2] # 2D
+        # input = tf.concat([discrim_inputs, discrim_targets], axis=3)
+        # 2x [batch, height, width, depth, in_channels] => [batch, height, width, depth, in_channels * 2]
+        input = tf.concat([discrim_inputs, discrim_targets], axis=4)
 
         # layer_1: [batch, 256, 256, in_channels * 2] => [batch, 128, 128, ndf]
+        # ADD one dimension in ALL comments for depth like in 'create_generator'
         with tf.variable_scope("layer_1"):
             convolved = discrim_conv(input, a.ndf, stride=2)
-            rectified = lrelu(convolved, 0.2)
+            # rectified = lrelu(convolved, 0.2)
+            rectified = rainersReLU(convolved)
             layers.append(rectified)
 
         # layer_2: [batch, 128, 128, ndf] => [batch, 64, 64, ndf * 2]
@@ -498,7 +508,8 @@ def create_model(inputs, targets):
                 stride = 1 if i == n_layers - 1 else 2  # last layer here has stride 1
                 convolved = discrim_conv(layers[-1], out_channels, stride=stride)
                 normalized = batchnorm(convolved)
-                rectified = lrelu(normalized, 0.2)
+                # rectified = lrelu(normalized, 0.2)
+                rectified = rainersReLU(normalized)
                 layers.append(rectified)
 
         # layer_5: [batch, 31, 31, ndf * 8] => [batch, 30, 30, 1]
@@ -517,12 +528,14 @@ def create_model(inputs, targets):
     # they share the same underlying variables
     with tf.name_scope("real_discriminator"):
         with tf.variable_scope("discriminator"):
-            # 2x [batch, height, width, channels] => [batch, 30, 30, 1]
+            # 2x [batch, height, width, channels] => [batch, 30, 30, 1] # 2D
+            # 2x [batch, height, width, depth, channels] => [batch, 30, 30, 30, 1]
             predict_real = create_discriminator(inputs, targets)
 
     with tf.name_scope("fake_discriminator"):
         with tf.variable_scope("discriminator", reuse=True):
-            # 2x [batch, height, width, channels] => [batch, 30, 30, 1]
+            # 2x [batch, height, width, channels] => [batch, 30, 30, 1] # 2D
+            # 2x [batch, height, width, depth, channels] => [batch, 30, 30, 30, 1]
             predict_fake = create_discriminator(inputs, outputs)
 
     with tf.name_scope("discriminator_loss"):
@@ -570,7 +583,7 @@ def create_model(inputs, targets):
     )
 
 
-def save_images(fetches, step=None):
+def save_images(fetches, step=None, ax=):
     image_dir = os.path.join(a.output_dir, "images")
     if not os.path.exists(image_dir):
         os.makedirs(image_dir)
@@ -587,12 +600,14 @@ def save_images(fetches, step=None):
             out_path = os.path.join(image_dir, filename)
             contents = fetches[kind][i]
             with open(out_path, "wb") as f:
-                f.write(contents)
+                # f.write(contents) # this does only work in 2D case
+                # save a max-projection along specified axes, default=2
+                f.write(np.max(contents, axis=ax))
         filesets.append(fileset)
     return filesets
 
 
-def append_index(filesets, step=False):
+def append_index(filesets, step=False, ax=2):
     index_path = os.path.join(a.output_dir, "index.html")
     if os.path.exists(index_path):
         index = open(index_path, "a")
@@ -611,11 +626,15 @@ def append_index(filesets, step=False):
         index.write("<td>%s</td>" % fileset["name"])
 
         for kind in ["inputs", "outputs", "targets"]:
-            index.write("<td><img src='images/%s'></td>" % fileset[kind])
+            # this does only work in 2D case
+            # index.write("<td><img src='images/%s'></td>" % fileset[kind])
+            # write a max-projection along specified axes, default=2
+            index.write("<td><img src='images/%s'></td>" % np.max(fileset[kind], axis=ax))
 
         index.write("</tr>")
     return index_path
 
+############### BIS HIER HER GEKOMMEN! ##################
 
 def main():
     if a.seed is None:
